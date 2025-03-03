d745b1639ffdd59b5837d183a1000a5bc9da3b54
Vinayak Dev <vinayakdev.sci@gmail.com>
2025-03-03 08:49:29 -0600

    Implement awaitable sleep method

--- coro.cpp
+++ coro.cpp
@@ -1,4 +1,9 @@
 #include "coro.h"
+#include <chrono>
+
+using sclock = std::chrono::high_resolution_clock;
+using time_point = std::chrono::time_point<sclock>;
+using ms = std::chrono::milliseconds;
 
 /// Produces an infinite stream of numbers.
 Generator getGenerator() {
@@ -13,10 +18,31 @@ GenericGenerator<int32_t> getGenericGenerator() {
   }
 }
 
+Sleep sleep_(double time) {
+  const auto start = sclock::now();
+  for (unsigned i = 0;; ++i) {
+    auto now = decltype(start)::clock::now();
+    // auto duration = ms{time};
+    struct args arguments = {
+        .now = now,
+        .start = start,
+        .duration = time,
+    };
+
+    co_yield arguments;
+  }
+}
+
 int main() {
   auto generator = getGenericGenerator();
-  for (int32_t i = 0; i < 20; ++i) {
+  auto sleeper = sleep_(0.09);
+
+  for (int32_t i = 0; i < 200; ++i) {
     std::cout << "Counter: " << generator() << "\n";
+    if (sleeper()) {
+      std::cout << "Lapsed time\n";
+      break;
+    }
   }
   return 0;
 }
--- coro.h
+++ coro.h
@@ -1,26 +1,39 @@
+#include <chrono>
 #include <coroutine>
 #include <exception>
 #include <iostream>
-#include <thread>
 
-template<typename PromiseType>
-struct GetPromise {
-    /// Implements  `promise` type that will be used by all the coroutine_handles.
-    PromiseType *p_;
-    // All the functions below are recognised/required by the compiler.
-    // Await ready returning false means that the function is immediately ready to suspend.
-    // This will make the first await_suspend after calling await_ready return true;
-    bool await_ready () { return false; }
-    // Await suspend returning false means that after all the tasks in the scope of the function
-    // are done, do _not_ suspend the coroutine. Here it accepts a coro handle and sets an internal
-    // pointer to point to its address. It is legal for await_suspend to return the coroutine handle from p_.
-    bool await_suspend(std::coroutine_handle<PromiseType> handle) {
-        p_ = &handle;
-        return false;
-    }
-    // Since await_suspend did not suspend execution and does not return anything in particular,
-    // we define an await_ready method to actually be able to access the promise_type from the struct.
-    PromiseType *await_resume() { return p_; }
+using sclock = std::chrono::high_resolution_clock;
+using time_point = std::chrono::time_point<sclock>;
+using ms = std::chrono::milliseconds;
+using sec = std::chrono::duration<double>;
+
+struct args {
+  time_point now;
+  time_point start;
+  double duration;
+};
+
+template <typename PromiseType> struct GetPromise {
+  /// Implements  `promise` type that will be used by all the coroutine_handles.
+  PromiseType *p_;
+  // All the functions below are recognised/required by the compiler.
+  // Await ready returning false means that the function is immediately ready to
+  // suspend. This will make the first await_suspend after calling await_ready
+  // return true;
+  bool await_ready() { return false; }
+  // Await suspend returning false means that after all the tasks in the scope
+  // of the function are done, do _not_ suspend the coroutine. Here it accepts a
+  // coro handle and sets an internal pointer to point to its address. It is
+  // legal for await_suspend to return the coroutine handle from p_.
+  bool await_suspend(std::coroutine_handle<PromiseType> handle) {
+    p_ = &handle;
+    return false;
+  }
+  // Since await_suspend did not suspend execution and does not return anything
+  // in particular, we define an await_ready method to actually be able to
+  // access the promise_type from the struct.
+  PromiseType *await_resume() { return p_; }
 };
 
 /// Generator is a simple generator that makes use of coroutines.
@@ -68,7 +81,7 @@ template <typename T> struct GenericGenerator {
     std::suspend_always initial_suspend() { return {}; }
     std::suspend_always final_suspend() noexcept { return {}; }
     // Set the excpetion ptr to point to the current exception. We will create a
-    // method that will rethrow this excpetion.
+    // method that will rethrow this exception.
     void unhandled_exception() { exception_ = std::current_exception(); }
     // Use a C++20 concept to assign a constraint on the template to choose the
     // correct overload.
@@ -117,4 +130,68 @@ private:
       full_ = true;
     }
   }
+};
+
+/// Sleep implements an awaitable sleep implementation.
+struct Sleep {
+  struct promise_type;
+  using handle_type = std::coroutine_handle<promise_type>;
+
+  struct promise_type {
+    bool lapsed_ = false;
+    // Usual boilerplate.
+    std::suspend_never initial_suspend() { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void unhandled_exception() {}
+
+    Sleep get_return_object() {
+      return Sleep(handle_type::from_promise(*this));
+    }
+
+    // We use yield_value here and not await_suspend outside because we
+    // need to maintain some internal state about how long the execution
+    // lasts. This is not the most elegant solution, but it works best.
+    std::suspend_always yield_value(struct args arguments) {
+      double elapsed_time = std::chrono::duration<double, std::milli>(
+                                arguments.now - arguments.start)
+                                .count();
+      lapsed_ = (elapsed_time - arguments.duration) > 0;
+      return {};
+    }
+  };
+
+  Sleep(const Sleep &) = delete;
+  Sleep(handle_type h) : handle_(h) {}
+
+  ~Sleep() {
+    handle_.destroy();
+    std::cout << "Destroyed sleep timer and its coro handle\n";
+  }
+
+  explicit operator bool() {
+    fill();
+    return !handle_.done();
+  }
+
+  // Why do it like this? What this function does is the following:
+  // If the coro has not been executed yet, it will call the handle_
+  // to execute it, and then set the executed_ flag to true, indicating
+  // completed execution. The () operator will execute this function,
+  // and then clear the execution cache to make the coro ready for another
+  // execution.
+  bool operator()() {
+    fill();
+    executed_ = false;
+    return std::move(handle_.promise().lapsed_);
+  }
+
+private:
+  void fill() {
+    if (!executed_) {
+      handle_();
+      executed_ = true;
+    }
+  }
+  handle_type handle_;
+  bool executed_ = false;
 };
